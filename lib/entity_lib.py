5e5b4ad3681a9119f0c4ec9f3ba40e11214e2465
HeyGlee <150066232+HeyGlee@users.noreply.github.com>
2023-11-09 17:07:49 +0000

    Added comments to code
    (!PLEASE! check the code again)

--- lib/entity_lib.py
+++ lib/entity_lib.py
@@ -1,7 +1,7 @@
 import random
 
 
-class Entity:
+class Entity:    # this is the parent class for every character in the game
     def __init__(self, name, level, health, max_health, mana, max_mana):
         self.name = name
         self.level = level
@@ -10,43 +10,43 @@ class Entity:
         self.mana = mana
         self.max_mana = max_mana
         self.is_dead = False
-        self.moves = []
+        self.moves = []    # empty list used for the character's moves
 
-    def heal(self, amount: int) -> None:
-        heal = self.health + amount
+    def heal(self, amount: int) -> None:    # function to heal the character
+        heal = self.health + amount    # gets the added amount
         if heal > self.max_health:
-            heal -= heal - self.max_health
-        self.health = heal
+            heal -= heal - self.max_health    # subtracts heal by the amount over the users max health
+        self.health = heal    # adds the new health to the users health
 
     def use_move(self, move, opponent, func) -> None:
-        if self.mana <= 0:
+        if self.mana <= 0:    # if the character has 0 mana then it will just instantly return and not use the move
             return
-        try:
-            if bool(move.miss_chance <= random.randint(1, 100)):
+        try:   
+            if bool(move.miss_chance <= random.randint(1, 100)):    # if the move misses it will just return
                 return
-            elif bool(move.crit_chance <= random.randint(1, 100)):
+            elif bool(move.crit_chance <= random.randint(1, 100)):    # calculates for crit
                 move.damage *= 1.3
         except AttributeError:
             continue
-        move.use_move(self)
-        if move.move_type == "Healing":
+        move.use_move(self)    # uses the moves mana
+        if move.move_type == "Healing":    # checks for the type of move
             self.heal(move.amount)
         elif move.move_type == "Stat":
             move.stat_change(self)
         else:
-            move.damage = move.calculate_damage()
-            opponent.health -= move.damage
-            if not func:
+            move.damage = move.calculate_damage()    # <- ISSUE: function needs to be updated to include the attack and defense buffs
+            opponent.health -= move.damage    # deals the damage
+            if not func:    # <- Will be reviewed shortly (potential issue)
                 return
             opponent.check_dead(func)
 
-    def check_dead(self, func) -> None:
+    def check_dead(self, func) -> None:    # method to check if the character is dead, runs a function if they are
         if self.health <= 0:
             self.is_dead = True
             func()
 
 
-class Character(Entity):
+class Character(Entity):    # class for the user 
     def __init__(self, name, level, health, max_health, mana, max_mana, xp, coins, damage_x, defense_x,
                  helmet, chestplate, leggings, boots, weapon):
         super().__init__(name, level, health, max_health, mana, max_mana)
@@ -60,13 +60,13 @@ class Character(Entity):
         self.boots = boots
         self.weapon = weapon
 
-    def level_up(self, xp_required):
+    def level_up(self, xp_required):    # checks for a level up. the xp_required is a dictionary with each level and the amount of xp needed to level up. May need to add try!
         if self.xp >= xp_required[self.level + 1]:
             self.level += 1
 
 
-class Mob(Entity):
-    def __init__(self, name, level, health, max_health, mana, max_mana, coins_on_death, xp_dropped):
+class Mob(Entity):    # main mob class, i haven't added side effects into this game so there are no sub-classes e.g. skeleton or fire skeleton. currently just this.
+    def __init__(self, name, level, health, max_health, mana, max_mana, coins_on_death, xp_dropped):    # has extra prop. of the coins & xp dropped on death
         super().__init__(name, level, health, max_health, mana, max_mana)
         self.coins_on_death = coins_on_death 
         self.xp_dropped = xp_dropped
